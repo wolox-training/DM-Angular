0fc1e3fc403f33d3e1179da3082bf553efb967a9
Darvand <david.montoya@wolox.co>
2020-02-28 11:08:01 -0500

    PR Fix: Remove console log and add environments on .gitignore

--- wolox-books/src/app/services/user.service.ts
+++ wolox-books/src/app/services/user.service.ts
@@ -1,33 +1,30 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { Observable, throwError } from "rxjs";
+import { catchError } from "rxjs/operators";
 
-import { environment } from '../../environments/environment';
-import { User } from '../models/user.model';
+import { environment } from "../../environments/environment";
+import { User } from "../models/user.model";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class UserService {
-
   readonly ROOT_URL = environment.BACKEND_URL;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   createUser(user: User): Observable<any> {
-    console.log({ user });
-    return this.http.post(`${this.ROOT_URL}/api/v1/users`, { user })
-    .pipe(
-      catchError(this.handleError)
-    );
+    return this.http
+      .post(`${this.ROOT_URL}/api/v1/users`, { user })
+      .pipe(catchError(this.handleError));
   }
 
   private handleError(error: HttpErrorResponse) {
     console.error(error);
     if (error.status === 422) {
-      return throwError('No es posible procesar el formulario');
+      return throwError("No es posible procesar el formulario");
     }
-    return throwError('¡Sucedio un error inesperado!');
+    return throwError("¡Sucedio un error inesperado!");
   }
 }
